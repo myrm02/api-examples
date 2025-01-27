978ec546afd98ba7ffb3de2aeb3d01660a5224a6
myrm02 <94831119+myrm02@users.noreply.github.com>
2025-01-27 13:44:12 +0100

    Add more payload size

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+const bodyParser from 'body-parser'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+// Increase payload size limit to 100MB
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000");
