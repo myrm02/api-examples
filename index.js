a899f2151f9b3aba1369c0055cf534973c6da95f
myrm02 <94831119+myrm02@users.noreply.github.com>
2025-02-03 16:21:34 +0100

    Test formData only

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser'
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Increase payload size limit to 100MB
@@ -16,9 +16,9 @@ app.listen(5000, () => {
   console.log("server is listening on port 5000");
 });
 
-app.post("/send", (req, res) => {
-  res.json(req.body);
-});
+// app.post("/send", (req, res) => {
+//   res.json(req.body);
+// });
 
 app.post("/fileInput", (req, res) => {
   const formData = req.body;
